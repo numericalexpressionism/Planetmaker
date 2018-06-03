11a3a585f24dd7a562cba422bf2a88cdcb360258
numericalexpressionism <vadim.vorobyov.by@gmail.com>
2018-06-03 23:06:47 +0300

    improvements to texture making

--- Assets/Voronoi/VoronoiGraph.cs
+++ Assets/Voronoi/VoronoiGraph.cs
@@ -25,11 +25,27 @@ public class VoronoiGraph
     foreach (var node in _nodes)
     {
       var p = node.Value.centre;
+      var uv = GetUV(mainTexture, p);
 
-      int px = Mathf.RoundToInt((1 - ((p.x+180) / 360f)) * mainTexture.width);
-      int py = Mathf.RoundToInt((1 - (p.y / 180f)) * mainTexture.height);
+      int px = Mathf.FloorToInt(uv.x);
+      int py = Mathf.FloorToInt(uv.y);
       mainTexture.SetPixel(px, py, Color.red);
+
+      var edges = node.Value.GetEdges();
+      foreach (var edge in edges)
+      {
+        var src = GetUV(mainTexture, edge._one);
+        var dst = GetUV(mainTexture, edge._other);
+      }
     }
     mainTexture.Apply();
+  }
+
+  private Vector2 GetUV(Texture2D mainTexture, Vector2 latLng)
+  {
+    return new Vector2(
+      (1 - ((latLng.x + 180) / 360f)) * mainTexture.width,
+      (1 - (latLng.y / 180f)) * mainTexture.height
+      );
   }
 }
